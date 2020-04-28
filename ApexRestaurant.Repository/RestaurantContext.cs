b407af12e046ebd2a32b65dc0c5c3b2b9417efd6
ruchiagrawal663 <ruchi.agrawal@apexcollege.edu.np>
2020-04-28 22:13:02 +0545

    added dbset

--- ApexRestaurant.Repository/RestaurantContext.cs
+++ ApexRestaurant.Repository/RestaurantContext.cs
@@ -9,6 +9,12 @@ namespace ApexRestaurant.Repository
 
             }
     public DbSet<Customer> Customers { get; set; }
+    public DbSet<Meals> Meals { get; set; }
+        public DbSet<Meal_Dishes> MealDishes { get; set; }
+        public DbSet<Menus> Menus { get; set; }       
+        public DbSet<Menu_items> MenuItems { get; set; }
+        public DbSet<Staff> Staffs { get; set; }
+        public DbSet<Ref_Staff_Roles> StaffRoles { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
         { 
              base.OnModelCreating(modelBuilder); 
